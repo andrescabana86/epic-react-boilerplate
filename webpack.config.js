7862ef31453ce9dc93d8bfc61ecb6da4577a4eca
andrescabana86 <andrescabana86@gmail.com>
2020-10-26 22:19:59 -0300

    chore: downgrade typescript compiler to ES5
    Signed-off-by: andrescabana86 <andrescabana86@gmail.com>

--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,27 @@
 const path = require('path')
-
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 module.exports = {
-  mode: "none",
+  mode: "development",
+  devtool: "inline-source-map",
   entry: {
     app: path.join(__dirname, 'src', 'index.tsx')
   },
   target: "web",
   resolve: {
-    extensions: ['ts', 'tsx', 'js']
+    extensions: ['.ts', '.tsx', '.js']
   },
   module: {
     rules: [
-      {
-        test: '/\.tsx?$/',
-        use: 'ts-loader',
-        exclude: '/node_modules/'
-      }
+      { test: /\.tsx?$/, loader: "ts-loader", exclude: '/node_modules/' }
     ]
   },
   output: {
     filename: "[name].js",
     path: path.resolve(__dirname, 'build')
-  }
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, 'src', 'index.html')
+    })
+  ]
 }
