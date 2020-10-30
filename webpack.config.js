65dcb7a3c2c203eca7c2dbc9a9ad7531d36ca97a
andrescabana86 <andrescabana86@gmail.com>
2020-10-30 13:13:33 -0300

    chore: add sass pre-compiler
    Signed-off-by: andrescabana86 <andrescabana86@gmail.com>

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,10 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin')
+
+const OUTPUT_PATH = path.resolve(__dirname, 'build')
+const OUTPUT_FILENAME = "[name].js"
+
 module.exports = {
   mode: "development",
   devtool: "inline-source-map",
@@ -8,20 +13,34 @@ module.exports = {
   },
   target: "web",
   resolve: {
-    extensions: ['.ts', '.tsx', '.js']
+    extensions: ['.ts', '.tsx', '.js'],
+    plugins: [new TsconfigPathsPlugin()],
   },
   module: {
     rules: [
-      { test: /\.tsx?$/, loader: "ts-loader", exclude: '/node_modules/' }
+      { test: /\.tsx?$/, loader: "ts-loader", exclude: '/node_modules/' },
+      {
+        test: /\.sass$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          'sass-loader',
+        ],
+      },
     ]
   },
   output: {
-    filename: "[name].js",
-    path: path.resolve(__dirname, 'build')
+    filename: OUTPUT_FILENAME,
+    path: OUTPUT_PATH
   },
   plugins: [
     new HtmlWebpackPlugin({
       template: path.join(__dirname, 'src', 'index.html')
-    })
-  ]
+    }),
+  ],
+  devServer: {
+    contentBase: path.join(__dirname, 'build'),
+    compress: true,
+    port: 8080
+  }
 }
