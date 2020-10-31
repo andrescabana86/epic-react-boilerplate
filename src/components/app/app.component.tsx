0202d7390f1134c4ce57f3f287c8e0bf3ced903a
andrescabana86 <andrescabana86@gmail.com>
2020-10-30 23:40:49 -0300

    chore: add eslint and prettier
    Signed-off-by: andrescabana86 <andrescabana86@gmail.com>

--- src/components/app/app.component.tsx
+++ src/components/app/app.component.tsx
@@ -1,8 +1,8 @@
-import React, { PureComponent } from "react";
+import React, { PureComponent } from 'react'
 import './app.component.sass'
 
 export class App extends PureComponent {
   render() {
-    return <h1 className="app">Hello world this is vanina practicing</h1>
+    return <h1 className="app">Hello world this is practicing</h1>
   }
 }
--- webpack.config.js
+++ webpack.config.js
@@ -3,44 +3,40 @@ const HtmlWebpackPlugin = require('html-webpack-plugin')
 const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin')
 
 const OUTPUT_PATH = path.resolve(__dirname, 'build')
-const OUTPUT_FILENAME = "[name].js"
+const OUTPUT_FILENAME = '[name].js'
 
 module.exports = {
-  mode: "development",
-  devtool: "inline-source-map",
+  mode: 'development',
+  devtool: 'inline-source-map',
   entry: {
-    app: path.join(__dirname, 'src', 'index.tsx')
+    app: path.join(__dirname, 'src', 'index.tsx'),
   },
-  target: "web",
+  target: 'web',
   resolve: {
     extensions: ['.ts', '.tsx', '.js'],
     plugins: [new TsconfigPathsPlugin()],
   },
   module: {
     rules: [
-      { test: /\.tsx?$/, loader: "ts-loader", exclude: '/node_modules/' },
+      { test: /\.tsx?$/, loader: 'ts-loader', exclude: '/node_modules/' },
       {
         test: /\.sass$/,
-        use: [
-          'style-loader',
-          'css-loader',
-          'sass-loader',
-        ],
+        use: ['style-loader', 'css-loader', 'sass-loader'],
       },
-    ]
+    ],
   },
   output: {
     filename: OUTPUT_FILENAME,
-    path: OUTPUT_PATH
+    path: OUTPUT_PATH,
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: path.join(__dirname, 'src', 'index.html')
+      template: path.join(__dirname, 'src', 'index.html'),
     }),
   ],
   devServer: {
     contentBase: path.join(__dirname, 'build'),
     compress: true,
-    port: 8080
-  }
+    port: 8080,
+  },
 }
