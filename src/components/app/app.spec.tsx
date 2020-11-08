273b7cee97a40e29e357999d708e60d349d8d8f0
andrescabana86 <andrescabana86@gmail.com>
2020-11-08 19:21:31 -0300

    test: add specs to <App />

--- src/components/app/app.spec.tsx
+++ src/components/app/app.spec.tsx
@@ -5,8 +5,15 @@ import { App } from '@components/app/app'
 describe('<App />', () => {
   afterEach(cleanup)
 
-  it('should render app component', () => {
+  it('should render', () => {
     const { asFragment } = render(<App />)
     expect(asFragment()).toMatchSnapshot()
+  })
+
+  it('should show title -> Welcome to my boilerplate', function () {
+    const { getByTestId } = render(<App />)
+    const title = getByTestId('title')
+    expect(title).toHaveTextContent('Welcome to my boilerplate')
+    expect(title).toHaveClass('app__header-title')
   })
 })
